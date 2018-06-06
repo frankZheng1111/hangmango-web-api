982b9cde8a7a84748bc3ffc9ac274da53e8b9061
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-06 10:59:10 +0800

    Add more test

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -29,3 +29,27 @@ func TestValidationErrorResponse(t *testing.T) {
 	assert.Equal(t, 400, w.Code)
 	assert.Equal(t, "ParamsValidationError", respJson["msg"])
 }
+
+func TestCommonPanicHandle(t *testing.T) {
+	req, err := http.NewRequest("GET", "/test", nil)
+	if err != nil {
+		panic(err)
+	}
+	w := httptest.NewRecorder()
+
+	r := gin.Default()
+	r.GET("/test", CommonPanicHandle(func(c *gin.Context) {
+		panic("testError")
+	}))
+	r.ServeHTTP(w, req)
+
+	var respJson map[string]interface{}
+
+	decoder := json.NewDecoder(w.Body)
+	if err := decoder.Decode(&respJson); err != nil {
+		panic(err)
+	}
+
+	assert.Equal(t, 500, w.Code)
+	assert.Equal(t, "SERVER_ERROR", respJson["msg"])
+}
