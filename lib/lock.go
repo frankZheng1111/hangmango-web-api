879cb150809903bed373a56f701d9e2a94edf6d6
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-17 22:34:30 +0800

    Add redis lock

---
+++ lib/lock.go
@@ -0,0 +1,30 @@
+package lib
+
+import (
+	"time"
+)
+
+func GetRedisLock(key string, expiredIn time.Duration) (int64, error) {
+	timestamp := time.Now().UnixNano()
+	set, err := Client.SetNX(key, timestamp, expiredIn).Result()
+	if err != nil {
+		return 0, err
+	}
+	if set {
+		return timestamp, nil
+	}
+	return 0, nil
+}
+
+func UnlockRedisLock(key string, value int64) (bool, error) {
+	luaScript := `
+		if (redis.call('get', KEYS[1]) == ARGV[1]) then
+			redis.call('del', KEYS[1])\n");
+			return true
+		else
+			return false
+		end
+	`
+	result, err := Client.Eval(luaScript, []string{key}, value).Result()
+	return result.(bool), err
+}
